b8fcad84520511a1cc482b1a4bf142ec09894131
Sainath Chilkuri <chilkurisainath@gmail.com>
2025-11-10 11:27:15 +0530

    Made Nav Back Stack generic

--- composeApp/src/commonMain/kotlin/org/demo/cmp/project/presentation/navigations/CustomNavGraph.kt
+++ composeApp/src/commonMain/kotlin/org/demo/cmp/project/presentation/navigations/CustomNavGraph.kt
@@ -2,6 +2,7 @@ package org.demo.cmp.project.presentation.navigations
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -12,7 +13,6 @@ import com.get.set.coremodule.navigations.Screens
 import org.demo.cmp.project.presentation.screens.dashboard.DashboardScreen
 import com.get.set.auth.presentation.login.LoginScreen
 import com.get.set.coremodels.models.UserDataModel
-import com.get.set.coremodule.JsonSerializerUtil
 import org.demo.cmp.project.presentation.screens.splash.SplashScreen
 import org.koin.core.Koin
 
@@ -30,13 +30,10 @@ fun CustomNavGraph(navController: NavHostController, koin: Koin) {
 
           composable(Screens.Dashboard.route,
               arguments = listOf(navArgument("data") { type = NavType.StringType })) { backStackEntry ->
-              val encodedData: String? = backStackEntry.arguments?.getString("data")
-              encodedData?.let {
-                  val userDataModel = JsonSerializerUtil.parseFromJson<UserDataModel>(encodedData);
-                  DashboardScreen(dashboardViewModel = koin.get(), userDataModel = userDataModel).Draw()
+              NavBackStackEntry.fetchData<UserDataModel>("data")?.let {
+                  DashboardScreen(dashboardViewModel = koin.get(), userDataModel = it).Draw()
               }
           }
-
 
       }
   }
---
+++ composeApp/src/commonMain/kotlin/org/demo/cmp/project/presentation/navigations/NavBackStackEntryUtil.kt
@@ -0,0 +1,13 @@
+package org.demo.cmp.project.presentation.navigations
+
+import androidx.navigation.NavBackStackEntry
+import com.get.set.coremodule.JsonSerializerUtil
+
+inline fun <reified T> NavBackStackEntry.Companion.fetchData(fieldName: String) : T? {
+    val encodedData: String? = arguments?.getString(fieldName)
+    encodedData?.let {
+        val result: T = JsonSerializerUtil.parseFromJson<T>(encodedData);
+        return result;
+    }
+    return null;
+}
